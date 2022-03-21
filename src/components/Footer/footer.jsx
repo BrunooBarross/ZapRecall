e3daa297dc130f4bd17f8fa63f13f991c102df05
BrunooBarross <barrosbrunofelipe@gmail.com>
2022-03-21 11:09:57 -0300

    feat: ajustes layout conforme novo figma - alterações de mensagens do footer conclusão - separando Header em novo arquivo

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,8 +3,22 @@ function Footer(props){
     return(
         <footer className="footer">                  
             {textoFooter === undefined ? <span>{contador}/8 CONCLUÍDOS</span> : 
-                textoFooter === true ?  <span>errou</span> :
-                <span>/acertou tudo</span>
+                textoFooter === true ? 
+                <>
+                    <div className="footer-resultado">
+                        <img src="/img/sad.png" alt="parabéns" />
+                        <p>Putz...</p> 
+                    </div>
+                    <span>Ainda faltam alguns...Mas não desanime!</span>
+                </>    
+                :
+                <>
+                    <div className="footer-resultado">
+                        <img src="/img/party.png" alt="parabéns" />
+                        <p>Parabéns!</p> 
+                    </div>
+                    <span>Você não esqueceu de nenhum flashcard!</span>
+                </>                    
             }   
             <div className="footer-icones">{icones}</div>     
         </footer>        
