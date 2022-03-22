06c1f7df5cd4f3ec6b0530e975378cf44cc1015c
Vinicius <vinigugelmin@hotmail.com>
2022-03-22 17:54:53 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./components/App"; 
-
-Renderizar();
-export default function Renderizar(){
-    ReactDOM.render(<App />, document.querySelector(".root"));
-}
-
